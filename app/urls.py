916f0f8b28c241166072148001546b4e3ea81a2b
Paulo H Freire <paulohenriquefmb@outlook.com>
2026-08-29 01:59:37 -0300

    feat(api): implementa views do CRUD e rotas

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from app.views import CategoriaViewSet, ProdutoViewSet
+
+router = DefaultRouter()
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'produtos', ProdutoViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from app.models import Categoria, Produto
+from app.api.serializers import CategoriaSerializer, ProdutoSerializer
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer    
